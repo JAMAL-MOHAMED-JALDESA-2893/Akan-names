0d3bb83f054a25058da15342cb143768e1b6b03b
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-07-17 23:07:21 +0300

    add an array:stored weekdays and male akan names in an array inside a function as variables

---
+++ script.js
@@ -0,0 +1,26 @@
+// Function that generates the akan names according to days of the week
+
+const akanNameGenerator = function () {
+  //Saved days of the week in an Array.
+  const weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  //Male Akan Names
+
+  const maleAkanNames = [
+    "Kwasi",
+    "Kwadwo",
+    "Kwabena",
+    "Kwaku",
+    "Yaw",
+    "Kofi",
+    "Kwame",
+  ];
+};
