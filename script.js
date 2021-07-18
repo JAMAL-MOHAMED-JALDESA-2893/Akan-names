c7afe755c6470e95788c9ecebb1c32e51eaa62cd
JAMAL MOHAMED <jamal.mohamed@student.moringaschool.com>
2021-07-18 18:12:21 +0300

    add js validation : added an if statement to validate the input of month value and date values on the form

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 // Function that generates the akan names according to days of the week
 
 const akanNameGenerator = function () {
@@ -66,6 +69,49 @@ const akanNameGenerator = function () {
   // variable For holding dayOfWeek of date Of Birth :aquired through subjecting the birthdate variable to a getDay() method. 
 
   const dayOfWeek = birthDate.getDay();
+
+
+ 
+
+
+  // Validating the form inputs using control structure if...else...statements.
+
+
+
+  // if (month < 1 || month > 12 || (month == 2 && date > 29)) {
+  //   return (document.getElementById("alert2").innerHTML =
+  //     "Invalid  Month ,Please Try Again");
+  // } else if (date < 1 || date > 31) {
+  //   return (document.getElementById("alert2").innerHTML =
+  //     "Invalid Date ,Please Try Again");
+  // } else if (year === NaN || year < 1900 || year > 2019) {
+  //   document.getElementById("alert2").innerHTML =
+  //     "Invalid year please try Again";
+  // }
+
+
+    
+
+     if (dateOfBirth < 1 || dateOfBirth > 31  ) {   //this condition validates date inputs only
+          swal(`Wrong date entered : please enter valid date`)
+          return;
+     } 
+     else if (monthOfBirth < 1 || monthOfBirth > 12 || (monthOfBirth == 2 && dateOfBirth > 29) ) {
+        swal("wrong month input : please enter valid month");
+        return;
+     } 
+     else if (dateOfBirth < 1 || dateOfBirth > 31  && monthOfBirth < 1 || monthOfBirth > 12 ) {
+        swal(`Wrong date entered : please enter valid date
+        wrong month entered : please enter valid month`)
+     };
+
+
+
+
+
+
+
+
 
   
 };
